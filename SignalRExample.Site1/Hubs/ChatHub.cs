1056f7541c605fee1c601a94554076730657816b
Tugay Kılavuzoğlu <tkilavuzoglu@bimser.com.tr>
2020-08-12 15:15:49 +0300

    Add project files.

---
+++ SignalRExample.Site1/Hubs/ChatHub.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+
+namespace SignalRExample.Site1.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            Console.WriteLine($"{Context.ConnectionId}-All-{message}");
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public async Task SendMessageToUser(string user, string targetConnectionId, string message)
+        {
+            Console.WriteLine($"{Context.ConnectionId}-{targetConnectionId}-{message}");
+            await Clients.User(targetConnectionId).SendAsync("ReceiveMessage", user, targetConnectionId, message);
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            Console.WriteLine(Context.ConnectionId);
+            return base.OnConnectedAsync();
+        }
+
+    }
+}
---
+++ SignalRExample.Site1/wwwroot/js/chat.js
@@ -0,0 +1,45 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+//Disable send button until connection is established
+document.getElementById("sendButton").disabled = true;
+
+//connection.on("ReceiveMessage", function (user, message) {
+//    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+//    var encodedMsg = user + " says " + msg;
+//    var li = document.createElement("li");
+//    li.textContent = encodedMsg;
+//    document.getElementById("messagesList").appendChild(li);
+//});
+
+connection.on("ReceiveMessage", function (user, targetConnectionId, message) {
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg = user + " says " + msg + "by" + targetConnectionId;
+    var li = document.createElement("li");
+    li.textContent = encodedMsg;
+    document.getElementById("messagesList").appendChild(li);
+});
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+    document.getElementById("userConnectionId").value = connection.connectionId;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+    var targetConnectionId = document.getElementById("targetConnectionId").value;
+    if (targetConnectionId) {
+        connection.invoke("SendMessageToUser", user, targetConnectionId, message).catch(function (err) {
+            return console.error(err.toString());
+        });
+    } else {
+        connection.invoke("SendMessage", user, message).catch(function (err) {
+            return console.error(err.toString());
+        });
+    }
+    event.preventDefault();
+});
