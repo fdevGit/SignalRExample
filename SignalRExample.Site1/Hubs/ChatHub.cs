85a8e8c47d4395aaa0acaec76abeb72e7e37061f
Tugay Kılavuzoğlu <tkilavuzoglu@bimser.com.tr>
2020-08-13 08:53:40 +0300

    #Ticket milestone

--- SignalRExample.Site1/Hubs/ChatHub.cs
+++ SignalRExample.Site1/Hubs/ChatHub.cs
@@ -6,12 +6,17 @@ namespace SignalRExample.Site1.Hubs
 {
     public class ChatHub : Hub
     {
-        public async Task SendMessage(string user, string message)
+        public async Task SendMessageToAll(string user, string message)
         {
             Console.WriteLine($"{Context.ConnectionId}-All-{message}");
             await Clients.All.SendAsync("ReceiveMessage", user, message);
         }
 
+        public async Task SendMessageToMe(string user, string message)
+        {
+            Console.WriteLine($"{Context.ConnectionId}-Caller-{message}");
+            await Clients.Caller.SendAsync("ReceiveMessage", user, message);
+        }
         public async Task SendMessageToUser(string user, string targetConnectionId, string message)
         {
             Console.WriteLine($"{Context.ConnectionId}-{targetConnectionId}-{message}");
@@ -20,8 +25,14 @@ namespace SignalRExample.Site1.Hubs
 
         public override Task OnConnectedAsync()
         {
-            Console.WriteLine(Context.ConnectionId);
+            Clients.All.SendAsync("Connect", Context.ConnectionId).Wait();
             return base.OnConnectedAsync();
+        }
+
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            Clients.All.SendAsync("Disconnect", Context.ConnectionId).Wait();
+            return base.OnDisconnectedAsync(exception);
         }
 
     }
--- SignalRExample.Site1/wwwroot/js/chat.js
+++ SignalRExample.Site1/wwwroot/js/chat.js
@@ -5,13 +5,37 @@ var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
 //Disable send button until connection is established
 document.getElementById("sendButton").disabled = true;
 
-//connection.on("ReceiveMessage", function (user, message) {
-//    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
-//    var encodedMsg = user + " says " + msg;
-//    var li = document.createElement("li");
-//    li.textContent = encodedMsg;
-//    document.getElementById("messagesList").appendChild(li);
-//});
+connection.on("Connect", function (loggedUserConnectionId) {
+    var option = document.createElement('option');
+    if (connection.connectionId == loggedUserConnectionId) {
+        for (var i = 0; i < document.getElementById("targetConnectionId").options.length; i++) {
+            if (document.getElementById("targetConnectionId").options[i].value == loggedUserConnectionId) {
+                document.getElementById("targetConnectionId").remove(i)
+            }
+        }
+        option.innerHTML = "Me";
+        option.value = loggedUserConnectionId;
+    } else {
+        option.text = option.value = loggedUserConnectionId;
+    }
+    document.getElementById("targetConnectionId").add(option);
+});
+
+connection.on("Disconnect", function (loggedUserConnectionId) {
+    for (var i = 0; i < document.getElementById("targetConnectionId").options.length; i++) {
+        if (document.getElementById("targetConnectionId").options[i].value == loggedUserConnectionId) {
+            document.getElementById("targetConnectionId").remove(i)
+        }
+    }
+});
+
+connection.on("ReceiveMessage", function (user, message) {
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg = user + " says " + msg;
+    var li = document.createElement("li");
+    li.textContent = encodedMsg;
+    document.getElementById("messagesList").appendChild(li);
+});
 
 connection.on("ReceiveMessage", function (user, targetConnectionId, message) {
     var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
@@ -33,11 +57,17 @@ document.getElementById("sendButton").addEventListener("click", function (event)
     var message = document.getElementById("messageInput").value;
     var targetConnectionId = document.getElementById("targetConnectionId").value;
     if (targetConnectionId) {
-        connection.invoke("SendMessageToUser", user, targetConnectionId, message).catch(function (err) {
-            return console.error(err.toString());
-        });
+        if (connection.connectionId == targetConnectionId) {
+            connection.invoke("SendMessageToMe", targetConnectionId, message).catch(function (err) {
+                return console.error(err.toString());
+            });
+        } else {
+            connection.invoke("SendMessageToUser", user, targetConnectionId, message).catch(function (err) {
+                return console.error(err.toString());
+            });
+        }
     } else {
-        connection.invoke("SendMessage", user, message).catch(function (err) {
+        connection.invoke("SendMessageToAll", user, message).catch(function (err) {
             return console.error(err.toString());
         });
     }
